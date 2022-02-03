e774429a77ddf0a09abc59398b834410183951b7
williamtbranch <williamtbranch@gmail.com>
2022-02-03 09:18:42 -0600

    added chart from tradingview

--- NODE/src/index.ts
+++ NODE/src/index.ts
@@ -1,5 +1,7 @@
 import {ipcRenderer} from "electron";
 import SecurityEndpoints, { securityDto } from "./services/SecurityEndpoints";
+import {createChart} from "./lightweight-charts.standalone.development.js";
+
 
 ipcRenderer.on('security:send', (event, arg) => {
   console.log("Got security from main process.");
@@ -13,5 +15,10 @@ ipcRenderer.on('catfact', (event, catfact: string) => {
 })
 
 ipcRenderer.on('security:get', (event, series: securityDto) => {
-  console.log(series); 
+  const chart = createChart(document.body, {width: 400, height: 300});
+  const lineSeries = chart.addLineSeries();
+  lineSeries.setData([
+    {time: '2019-04-11', value: 80.01},
+    { time: '2019-04-12', value: 96.63}
+  ]);
 })
